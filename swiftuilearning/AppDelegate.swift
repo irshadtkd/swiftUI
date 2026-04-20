29f1e669d6c2b7f99e5b39160dd55740b57784ef
inv-irshadali <irshadali@innovaturelabs.com>
2026-04-20 15:49:38 +0530

    refact: updated notifications feature

--- swiftuilearning/AppDelegate.swift
+++ swiftuilearning/AppDelegate.swift
@@ -2,31 +2,96 @@ import UIKit
 import UserNotifications
 import FirebaseAuth
 import FirebaseCore
+import FirebaseMessaging
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
-        requestNotificationPermission()
+        
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+        
         configureNavigationBarAppearance()
+        requestNotificationPermission()
+        
         return true
     }
-
-    private func configureNavigationBarAppearance() {
-        let white = UIColor.white
-        UINavigationBar.appearance().tintColor = white
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: white]
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: white]
+    
+    // MARK: - URL Handling (Required when FirebaseAppDelegateProxyEnabled = NO)
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        return Auth.auth().canHandle(url)
     }
     
+    // MARK: - APNs Token Registration
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Auth.auth().setAPNSToken(deviceToken, type: .unknown)
+        Messaging.messaging().apnsToken = deviceToken
+        print("APNs token registered: \(deviceToken)")
+    }
     
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        print("APNs registration failed: \(error.localizedDescription)")
+    }
+    
+    // MARK: - Remote Notification Handling
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+            return
+        }
+        completionHandler(.newData)
+    }
+    
+    // MARK: - FCM Token
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("FCM Token: \(fcmToken ?? "nil")")
+    }
+    
+    // MARK: - UNUserNotificationCenterDelegate
+    // Called when notification is received in foreground
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .badge, .sound])
+    }
+    
+    // Called when user taps on a notification
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        completionHandler()
+    }
+    
+    // MARK: - Private Helpers
     private func requestNotificationPermission() {
         UNUserNotificationCenter.current()
-            .requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            .requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+                if let error = error {
+                    print("Notification permission error: \(error.localizedDescription)")
+                    return
+                }
                 if granted {
                     DispatchQueue.main.async {
                         UIApplication.shared.registerForRemoteNotifications()
@@ -37,34 +102,10 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             }
     }
     
-    
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        print("deviceToken :\(deviceToken)")
-        Auth.auth().setAPNSToken(deviceToken, type: .unknown)
-        print("APNs token registered")
-    }
-    
-    
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-        print("APNs registration failed:", error.localizedDescription)
-    }
-    
-    
-    func application(
-        _ application: UIApplication,
-        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
-        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-    ) {
-        if Auth.auth().canHandleNotification(userInfo) {
-            completionHandler(.noData)
-            return
-        }
-        completionHandler(.newData)
+    private func configureNavigationBarAppearance() {
+        let white = UIColor.white
+        UINavigationBar.appearance().tintColor = white
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: white]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: white]
     }
 }
